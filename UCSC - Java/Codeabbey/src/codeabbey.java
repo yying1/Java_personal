57cd5b8d679c86bd3a03a2caa19e539f7df1c0f9
Frank Ying <yingyuefrank@hotmail.com>
2021-07-26 00:32:36 +0800

    codeabbey java problem 3

--- UCSC - Java/Codeabbey/src/codeabbey.java
+++ UCSC - Java/Codeabbey/src/codeabbey.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
-import java.util.ArrayList;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 public class codeabbey {
 	public static void p_1() {
@@ -31,9 +32,23 @@ public class codeabbey {
 		System.out.print(sum);
 		readInput.close();
 	}
-	public static void main(String[] args) {
+	
+	public static void p_3() throws FileNotFoundException {
+		// for scanner read file throws filenot found exception is a must: https://stackoverflow.com/questions/19307622/java-says-filenotfoundexception-but-file-exists 
+		 File f = new File("3.txt");
+		 System.out.println(f.exists());
+		 System.out.println(new File("3.txt").getAbsoluteFile());
+		 System.out.println(new File("3.txt").canRead());
+		 Scanner readFile = new Scanner(f);
+		 String line;
+		 line = readFile.nextLine();
+		 
+		 System.out.println(line);
+		 readFile.close();
+	}
+	public static void main(String[] args) throws FileNotFoundException {
 		// TODO Auto-generated method stub
-		p_2();
+		p_3();
 	}
 
 }
