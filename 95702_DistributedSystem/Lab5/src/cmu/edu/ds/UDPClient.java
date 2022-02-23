eb3aacd15597cb674b1c3588ef247253dd7542ff
Frank Yue Ying <yingyuefrank@hotmail.com>
2022-02-23 16:48:32 -0500

    95702 Lab 5

--- 95702_DistributedSystem/Lab5/src/cmu/edu/ds/UDPClient.java
+++ 95702_DistributedSystem/Lab5/src/cmu/edu/ds/UDPClient.java
@@ -5,7 +5,9 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
+import java.util.Random;
 import java.util.Scanner;
+import java.math.BigInteger;
 
 /*
 Based on Coulouris UDP socket code
@@ -14,18 +16,31 @@ public class UDPClient {
     private DatagramSocket socket = null;
     private InetAddress host = null;
     private int port;
+    public static int g = 5;
+    public static final BigInteger a = new BigInteger(2046, new Random());
+    public static String p_l = "29455831888140518076474747925200735831996087523515089351305710049596033526238163973239362438299187714861164059458306537966923189121483309380193812391176324371821404328306009372066904964918195671218905191626038217661724017471173451035247796271257458369077948625384652200912648231914498423025647630580939224343513672606007162748159635064224151355895492579269319645649832605784649395525556834728089381127209558678357734944513106656109663590831330308952641905250879634739131347332611006943303916994576338027395880915575015414772552163574891795233906609342414029668068533356545578107870365635398276428848740477292742280559";
+    public static BigInteger p = new BigInteger(p_l);
+    //public static int a = 2;
+    //public static int p = 23;
 
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
-        System.out.print("Enter an integer: ");
-        int value = Integer.parseInt(keyboard.nextLine());
-        System.out.println("value = " + value);
+        BigInteger value;
+        //System.out.print("Enter an integer: ");
+        //int value = Integer.parseInt(keyboard.nextLine());
+        //System.out.println("value = " + value);
         UDPClient udpClient = new UDPClient();
         udpClient.init("localhost", 7272);
-        udpClient.send(Integer.toString(value));
-        value = Integer.parseInt(udpClient.receive());
-        System.out.println("Answer: " + value);
+        //udpClient.send(Integer.toString(value));
+        //System.out.println(BigInteger.valueOf(g).modPow(a,p));
+        udpClient.send(String.valueOf(BigInteger.valueOf(g).modPow(a,p)));
+        //System.out.println(a);
+        value = udpClient.receive();
+        System.out.println("Bob responded: " + value);
+        BigInteger shared_secret = value.modPow(a,p);
+        System.out.println("Shared secrete: " + shared_secret);
         udpClient.close();
+        //System.out.println(a);
     }
 
     private void init(String hostname, int portNumber) {
@@ -50,15 +65,16 @@ public class UDPClient {
         }
     }
 
-    private String receive() {
-        byte[] answer = new byte[256];
+    private BigInteger receive() {
+        byte[] answer = new byte[10000];
         DatagramPacket reply = new DatagramPacket(answer, answer.length);
         try {
             socket.receive(reply);
         } catch (IOException e) {
             System.out.println("IO error " + e.getMessage());
         }
-        return(new String(reply.getData(), 0, reply.getLength()));
+        String result = new String(reply.getData(), 0, reply.getLength());
+        return new BigInteger(result);
 
     }
 
--- 95702_DistributedSystem/Lab5/src/cmu/edu/ds/UDPServer.java
+++ 95702_DistributedSystem/Lab5/src/cmu/edu/ds/UDPServer.java
@@ -1,10 +1,12 @@
 package cmu.edu.ds;
 
 import java.io.IOException;
+import java.math.BigInteger;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
+import java.util.Random;
 
 /*
 Based on Coulouris UDP socket code
@@ -13,15 +15,24 @@ public class UDPServer {
     private DatagramSocket socket = null;
     private InetAddress inetAddress = null;
     private int port;
+    public static int g = 5;
+    public static final BigInteger b = new BigInteger(2046, new Random());
+    public static String p_l = "29455831888140518076474747925200735831996087523515089351305710049596033526238163973239362438299187714861164059458306537966923189121483309380193812391176324371821404328306009372066904964918195671218905191626038217661724017471173451035247796271257458369077948625384652200912648231914498423025647630580939224343513672606007162748159635064224151355895492579269319645649832605784649395525556834728089381127209558678357734944513106656109663590831330308952641905250879634739131347332611006943303916994576338027395880915575015414772552163574891795233906609342414029668068533356545578107870365635398276428848740477292742280559";
+    public static BigInteger p = new BigInteger(p_l);
+    //public static int b = 8;
+    //public static int p = 23;
 
     public static void main(String[] args) {
         UDPServer udpServer = new UDPServer();
         udpServer.init(7272);
-        int value = Integer.parseInt(udpServer.receive());
-        System.out.println("Server received: " + value);
-        value++;
-        String message = Integer.toString(value);
+        BigInteger value = udpServer.receive();
+        System.out.println("Bob received: " + value);
+        String message = String.valueOf(BigInteger.valueOf(g).modPow(b,p));
+        //System.out.println(b);
         udpServer.send(message);
+        BigInteger shared_secret = value.modPow(b,p);
+        System.out.println("Shared secrete: " + shared_secret);
+        //System.out.println(b);
         udpServer.close();
     }
 
@@ -37,7 +48,7 @@ public class UDPServer {
     }
 
     private void send(String message) {
-        byte[] buffer = new byte[256];
+        byte[] buffer = new byte[100000];
         buffer = message.getBytes();
         DatagramPacket reply = new DatagramPacket(buffer, buffer.length, inetAddress, port);
         try {
@@ -50,8 +61,8 @@ public class UDPServer {
 
     }
 
-    private String receive() {
-        byte[] buffer = new byte[256];
+    private BigInteger receive() {
+        byte[] buffer = new byte[100000];
         DatagramPacket request = new DatagramPacket(buffer, buffer.length);
 
         try {
@@ -63,7 +74,8 @@ public class UDPServer {
         } catch (IOException e) {
             System.out.println("IO error " + e.getMessage());
         }
-        return new String(request.getData(), 0, request.getLength());
+        String result = new String(request.getData(), 0, request.getLength());
+        return new BigInteger(result);
     }
 
     private void close() {
