515a12d84f465fc8a61a3a3ac258db674f534d41
Frank Yue Ying <yingyuefrank@hotmail.com>
2022-02-24 09:39:38 -0500

    Project 2 task 3

--- 95702_DistributedSystem/Project2Task3/src/main/java/RemoteVariableClientUDP.java
+++ 95702_DistributedSystem/Project2Task3/src/main/java/RemoteVariableClientUDP.java
@@ -1,6 +1,5 @@
-import java.io.BufferedReader;
+
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.*;
 import java.util.Scanner;
 
@@ -8,30 +7,43 @@ public class RemoteVariableClientUDP {
 	static int serverPort = 6789;
 	//setup socket and request datagram
 	static DatagramSocket aSocket = null;
-	static String nextLine;
+	static String message;
+	public static String ID;
 
-    public static void main(String args[]) throws IOException {
+    public static void main(String[] args) throws IOException {
 
-	// Add console output indicating status
-	System.out.println("The client is running.");
+		// Add console output indicating status
+		System.out.println("The client is running.");
 
-	//collecting port number
-	System.out.println("Please enter server port:");
-	Scanner readline = new Scanner(System.in);
-	serverPort = Integer.parseInt(readline.nextLine().trim());
-	BufferedReader typed = new BufferedReader(new InputStreamReader(System.in));
-	while ((nextLine = typed.readLine()) != null) {
-		if (nextLine.equals("halt!")) {
-			System.out.println("Client side quitting");
-			break;
-		}else {
-			int result = add(Integer.parseInt(nextLine));
-			System.out.println("The server returned: " + result);
+		//collecting port number
+		System.out.println("Please enter server port:");
+		Scanner readline = new Scanner(System.in);
+		serverPort = Integer.parseInt(readline.nextLine().trim());
+		while(true) {
+			System.out.println("1. Add a value to your sum." + "\r\n" + "2. Subtract a value from your sum.\r\n" + "3. Get your sum.\r\n" + "4. Exit client");
+			int option = Integer.valueOf(readline.nextLine());
+			if (option == 4){
+				System.out.println("Client side quitting. The remote variable server is still running.");
+				break;
+			}
+			int value = 0;
+			if (option == 1) {
+				System.out.println("Enter your value to add:");
+			} else if (option == 2) {
+				System.out.println("Enter your value to subtract:");
+			}
+			if (!(option == 3)) {
+				value = Integer.valueOf(readline.nextLine());
+			}
+			System.out.println("Enter your ID:");
+			ID = readline.nextLine();
+			message = option + " " + ID + " " + value;
+			int result = request(message);
+			System.out.println("The result is " + result);
 		}
 	}
-    }
 
-    public static int add(int i) {
+    public static int request(String i) {
 		//create variables to be used as socket and buffer data later
 
 		DatagramPacket reply = null;
@@ -42,7 +54,7 @@ public class RemoteVariableClientUDP {
 			aSocket = new DatagramSocket();
 
 			//read input with BufferedReader
-			byte[] m = String.valueOf(i).getBytes();
+			byte[] m = i.getBytes();
 			DatagramPacket request = new DatagramPacket(m, m.length, aHost, serverPort);
 
 			//send request
--- 95702_DistributedSystem/Project2Task3/src/main/java/RemoteVariableServerUDP.java
+++ 95702_DistributedSystem/Project2Task3/src/main/java/RemoteVariableServerUDP.java
@@ -2,10 +2,11 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.SocketException;
+import java.util.Hashtable;
 
 public class RemoteVariableServerUDP {
-	static int sum = 0;
-	public static void main(String args[]){
+	public static Hashtable<String, Integer> sum_table = new Hashtable<String, Integer>();
+	public static void main(String args[]) throws IOException {
 	//Add console output indicating status
 	System.out.println("Server started.");
 
@@ -29,29 +30,27 @@ public class RemoteVariableServerUDP {
 			//convert request data to string with substring
 			String requestString = new String(request.getData()).substring(0,request.getLength());
 
+			//break string into different parts
+			String[] requestString_array = requestString.split(" ");
+			String ID = requestString_array[1];
+			int option = Integer.parseInt(requestString_array[0]);
+			int value = Integer.parseInt(requestString_array[2]);
+			String operation = "Add";
+			if (option ==2){
+				operation = "Subtract";
+			} else if (option ==3){
+				operation = "Get";
+			}
 			//do calculation
-			int i = 0;
-			if (!requestString.equals("halt!")) {
-				i = Integer.parseInt(requestString);
-				System.out.println("Adding: "+requestString+" to "+String.valueOf(sum));
-				perform(i);
-			}
+			perform(option, ID,value);
+			System.out.println("ID: "+ID+", "+"Operation: "+operation+", Replied: "+sum_table.get(ID));
 
-			//adding stop mechanism with keyword halt!
-			if (requestString.equals("halt!")) {
-				//echo back request message
-				DatagramPacket reply = new DatagramPacket(request.getData(),
-						request.getLength(), request.getAddress(), request.getPort());
-				aSocket.send(reply);
-			} else {
-				//echo new value
+			//echo new value
 					//conversion from int to byte array referenced from https://stackoverflow.com/questions/2183240/java-integer-to-byte-array
-				buffer = String.valueOf(sum).getBytes();
-				DatagramPacket reply = new DatagramPacket(buffer, buffer.length, request.getAddress(), request.getPort());
-				aSocket.send(reply);
-				System.out.println("Returning sum of "+sum+" to client");
+			buffer = String.valueOf(sum_table.get(ID)).getBytes();
+			DatagramPacket reply = new DatagramPacket(buffer, buffer.length, request.getAddress(), request.getPort());
+			aSocket.send(reply);
 			}
-		}
 	//exception handling and cleanup
 	}catch (SocketException e){System.out.println("Socket: " + e.getMessage());
 	}catch (IOException e) {System.out.println("IO: " + e.getMessage());
@@ -59,8 +58,15 @@ public class RemoteVariableServerUDP {
 	}
 
 	//add method for separation of concern
-	public static void perform(int i) {
-		sum = sum+i;
+	public static void perform(int option, String ID, int i) {
+		if (!sum_table.containsKey(ID)){
+			sum_table.put(ID, 0);
+		}
+		if (option == 1){
+			sum_table.put(ID,sum_table.get(ID)+i);
+		}else if (option == 2){
+			sum_table.put(ID,sum_table.get(ID)-i);
+		}
 	}
 }
 
